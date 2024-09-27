628792e44baf73333b9d87dc6634ebb31fbd471c
Linh <ngtualinh2oo4@gmail.com>
2024-09-27 13:15:09 +0700

    update1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projekt1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
