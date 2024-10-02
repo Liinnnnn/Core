099c29c2b72c4e473d3090032cd17a7d51ae2c20
Linh <ngtualinh2oo4@gmail.com>
2024-10-02 16:27:30 +0700

    FixBugBin

--- obj/Debug/net8.0/projekt1.AssemblyInfo.cs
+++ obj/Debug/net8.0/projekt1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projekt1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a03cc663624e636a01812d35e55a88e20f344b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53ba4ccc028879b2dde4dc3e3d8b9cfac993bc32")]
 [assembly: System.Reflection.AssemblyProductAttribute("projekt1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projekt1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
