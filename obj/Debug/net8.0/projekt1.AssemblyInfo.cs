53b12fa3c0b7a130ff97703ba8106d89330aecb5
Linh <ngtualinh2oo4@gmail.com>
2024-10-26 08:09:57 +0700

    trivialfix

--- obj/Debug/net8.0/projekt1.AssemblyInfo.cs
+++ obj/Debug/net8.0/projekt1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projekt1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae13bcdc15fb2999bfd12db901abff6b410fc51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be3784325d1cd759a8be0b9b7be22a71e5047629")]
 [assembly: System.Reflection.AssemblyProductAttribute("projekt1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projekt1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
