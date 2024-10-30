da525f48803cd9baf29ae4384cc45f5795a25834
Linh <ngtualinh2oo4@gmail.com>
2024-10-30 20:52:03 +0700

    Uwu

--- obj/Debug/net8.0/projekt1.AssemblyInfo.cs
+++ obj/Debug/net8.0/projekt1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projekt1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07bb9c8644cec94536a0c96e1e2851dbe8064d81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a2d6563b99cba289aedac7311454b349ae0ac2")]
 [assembly: System.Reflection.AssemblyProductAttribute("projekt1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projekt1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
