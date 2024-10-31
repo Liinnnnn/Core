8d07f785c43f130b9b6431bc5c68b605dec50abe
Linh <ngtualinh2oo4@gmail.com>
2024-10-31 10:00:34 +0700

    Demo2

--- obj/Debug/net8.0/projekt1.AssemblyInfo.cs
+++ obj/Debug/net8.0/projekt1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projekt1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8dbdf59d73b0efe8487791bd0a27d5b40137633")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05fc8663d70ceb8f779c389e5840117ed4cf9f2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("projekt1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projekt1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
